f3c952fd3736786e65985f7126b043bb6127f412
Martin Hizkia <martinhizkia@gmail.com>
2021-10-01 22:35:32 +0700

    Add Filter.java

---
+++ goldenSample/Filter.java
@@ -0,0 +1,22 @@
+package goldenSample;
+
+import java.util.ArrayList;
+
+public class Filter {
+    public static ArrayList<PriceTag> filterPriceTag(PriceTag[] list, int value, boolean less) {
+        ArrayList<PriceTag> priceTags = new ArrayList<>();
+        for (PriceTag e : list) {
+            if (less && e.getAdjustedPrice() < value || !less && e.getAdjustedPrice() >= value)
+                priceTags.add(e);
+        }
+        return priceTags;
+    }
+
+    public static void filterProductRating(ArrayList<ProductRating> list, double value, boolean less) {
+        for (int i = 0; i < list.size(); ++i) {
+            final ProductRating e = list.get(i);
+            if (less && e.getAverage() < value || !less && e.getAverage() >= value)
+                list.remove(i);
+        }
+    }
+}
